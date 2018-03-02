c14ba570caaeaac8bcadce4b6c9844414582a95e
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-03-02 10:16:03 +0100

    Add scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+
+getTriangleArea - function getTriangleArea(a, h) {
+
+	if ((a > 0 ) && (h > 0)) {
+		return a*h/2;
+	} if ((a <= 0 ) && (h <= 0)) {
+		console.log('Nieprawidłowe dane');
+	}
+}
+console.log(getTriangleArea(10, 6));
